fba7ca1a85072e7f9bd1bb6e1fe9e8c058bf90df
EDkinGG <r23ghani@gmail.com>
2022-12-23 23:12:33 +0600

    Mara

--- Pomodoro/Home.swift
+++ Pomodoro/Home.swift
@@ -12,69 +12,97 @@ struct Home: View {
     
     @State var shouldShowLogOutOptions = false
     @Binding var isUserCurrentlyLoggedOut : Bool
-    @State var navigated = false
+    @State var navigatedPomo = false
+    @State var navigatedEdit = false
+    @State var navigatedAbout = false
     
     
     var body: some View {
+        
         NavigationView {
             
-            VStack(alignment: .leading, spacing: 4) {
-                NavigationLink("", destination: PomodoroMain(), isActive: $navigated)
-                Button(action: { self.navigated.toggle() },label: {
-                    Text("Start Pomodoro")
-                        .bold()
-                        .frame(width: 200, height: 40)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                .fill(.linearGradient(colors: [.pink, .red], startPoint: .top , endPoint: .bottomTrailing))
-                        )
-                        .foregroundColor(.white)
-                })
-                .padding(.top)
-                .offset(y: -10)
+            ScrollView{
                 
-                
-                
-                Button {
-                    //API Call
-                }label: {
-                    Text("Basic API")
-                        .bold()
-                        .frame(width: 200, height: 40)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                .fill(.linearGradient(colors: [.pink, .red], startPoint: .top , endPoint: .bottomTrailing))
-                        )
-                        .foregroundColor(.white)
+                VStack(alignment: .leading, spacing: 4) {
+                    NavigationLink("", destination: PomodoroMain(), isActive: $navigatedPomo)
+                    Button(action: { self.navigatedPomo.toggle() } ) {
+                        HStack{
+                            Image(systemName: "timer")
+                            Text("PomoDoro").font(.title)
+                        }
+                        .modifier(ButtonModifers())
+                        .padding()
+                    }
+                    
+    //                Button {
+    //                    //API Call
+    //                }label: {
+    //                    Text("Basic API")
+    //                        .bold()
+    //                        .frame(width: 200, height: 40)
+    //                        .background(
+    //                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+    //                                .fill(.linearGradient(colors: [.pink, .red], startPoint: .top , endPoint: .bottomTrailing))
+    //                        )
+    //                        .foregroundColor(.white)
+    //                }
+    //                .padding(.top)
+    //                .offset(y: 0)
+                    
+                    NavigationLink("", destination: EditProfileView(), isActive: $navigatedEdit)
+                    Button(action: {self.navigatedEdit.toggle()}){
+                        HStack{
+                            Image(systemName: "pencil")
+                            Text("Edit Profile").font(.title)
+                        }
+                        .modifier(ButtonModifers())
+                        .padding()
+                    }
+                    
+                    Button(action: {}){
+                        HStack{
+                            Image(systemName: "doc.text.image.fill")
+                            Text("About Us").font(.title)
+                        }
+                        .modifier(ButtonModifers())
+                        .padding()
+                    }
+                    
+                    
+                    Button {
+                        shouldShowLogOutOptions.toggle()
+                    } label: {
+                            HStack{
+                                Image(systemName: "rectangle.portrait.and.arrow.right.fill")
+                                Text("Log Out").font(.title)
+                            }
+                            .modifier(ButtonModifers())
+                            .padding()
+                        
+    //                    Text("Sign Out")
+    //                        .bold()
+    //                        .frame(width: 200, height: 40)
+    //                        .background(
+    //                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+    //                                .fill(.linearGradient(colors: [.pink, .red], startPoint: .top , endPoint: .bottomTrailing))
+    //                        )
+    //                        .foregroundColor(.white)
+                    }
+    //                .padding(.top)
+    //                .offset(y: 100)
+                    
                 }
-                .padding(.top)
-                .offset(y: 0)
-                Button {
-                    shouldShowLogOutOptions.toggle()
-                } label: {
-                    Text("Sign Out")
-                        .bold()
-                        .frame(width: 200, height: 40)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                .fill(.linearGradient(colors: [.pink, .red], startPoint: .top , endPoint: .bottomTrailing))
-                        )
-                        .foregroundColor(.white)
+                .padding()
+                .actionSheet(isPresented: $shouldShowLogOutOptions) {
+                    .init(title: Text("Settings"), message: Text("What do you want to do?"), buttons: [
+                        .destructive(Text("Sign Out"), action: {
+                            print("handle sign out")
+                            try? Auth.auth().signOut()
+                            self.isUserCurrentlyLoggedOut = false
+                        }),
+                        .cancel()
+                    ])
                 }
-                .padding(.top)
-                .offset(y: 100)
-                
-            }
-            .padding()
-            .actionSheet(isPresented: $shouldShowLogOutOptions) {
-                .init(title: Text("Settings"), message: Text("What do you want to do?"), buttons: [
-                    .destructive(Text("Sign Out"), action: {
-                        print("handle sign out")
-                        try? Auth.auth().signOut()
-                        self.isUserCurrentlyLoggedOut = false
-                    }),
-                    .cancel()
-                ])
             }
         }
     }
--- Pomodoro/Views/EditProfileView.swift
+++ Pomodoro/Views/EditProfileView.swift
@@ -6,21 +6,139 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditProfileView: View {
+    @State private var uname = ""
+    @State private var mail = ""
+    @State private var address = ""
+    @State private var phone = ""
+    @State private var age = ""
+    
+    @State private var profileImage: Image?
+    
+    @State private var pickedImage: Image?
+    @State private var showingActionSheet = false
+    @State private var showingImagePicker = false
+    @State private var imageData: Data = Data()
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    
+    @StateObject var photoPicker = PhotoPicker()
+    
+    
+//    func loadImage() {
+//        guard let inputImage = pickedImage else
+//        {
+//            return
+//        }
+//        profileImage = inputImage
+//    }
     
     
     
     var body: some View {
-        VStack{
-            Text("Hello, Taj!")
-            Text("Hello, Amdad!")
-            
-            HStack{
-                Text("Hello, Rashed!")
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            }
+        
+        NavigationStack{
+//            ScrollView{
+                VStack
+                {
+                        VStack{
+                            if let image = photoPicker.image {
+                                image
+                                .resizable()
+                                .clipShape(Circle())
+                                .frame(width: 150, height: 150)
+                                .padding(.top, 20)
+                                } else {
+                                    Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .clipShape(Circle())
+                                    .frame(width: 150, height: 150)
+                                    .padding(.top, 20)
+                            }
+                        }
+                        .padding()
+                        .navigationTitle("Profile Details")
+                        .font(.system(size: 32, weight: .heavy))
+                        .toolbar{
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                PhotosPicker(selection: $photoPicker.imageSelection,
+                                             matching: .images,
+                                             photoLibrary: .shared()) {
+                                    Image(systemName: "photo")
+                                        .imageScale(.large)
+                                }
+                            }
+                        }
+
+                    
+//                    VStack{
+//                        Group{
+//
+//                            if
+//
+//                            if profileImage != nil {
+//                                profileImage!.resizable()
+//                                    .clipShape(Circle())
+//                                    .frame(width: 150, height: 150)
+//                                    .padding(.top, 20)
+//                                    .onTapGesture{
+//                                        showingActionSheet.toggle()
+//                                    }
+//                            } else {
+//                                Image(systemName: "person.circle.fill")
+//                                    .resizable()
+//                                    .clipShape(Circle())
+//                                    .frame(width: 150, height: 150)
+//                                    .padding(.top, 20)
+//                                    .onTapGesture{
+//                                        showingActionSheet.toggle()
+//                                    }
+//
+//                            }
+//                        }
+//                    }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage){
+//                        ImagePicker(pickedImage: self.$pickedImage, showImagePicker: self.$showingImagePicker, imageData: self.$imageData)
+//                    }.actionSheet(isPresented: $showingActionSheet ) {
+//                        ActionSheet(title: Text(""), buttons: [
+//                            .default(Text("Choose a Photo")){
+//                                self.sourceType = .photoLibrary
+//                                self.showingImagePicker = true
+//                            },
+//                            .default(Text("Take A Photo")){
+//                                self.sourceType = .camera
+//                                self.showingImagePicker = true
+//                            }, .cancel()
+//                        ])
+//                    }
+                    
+//                    Group{
+                        FormField(value: $uname, icon: "person.fill", placeholder: "UserName")
+                        
+                        FormField(value: $uname, icon: "person.crop.square.filled.and.at.rectangle.fill", placeholder: "E-mail")
+                        
+                        FormField(value: $uname, icon: "location.north.circle.fill", placeholder: "Address")
+                        
+                        FormField(value: $uname, icon: "phone.fill", placeholder: "Phone")
+                        
+//                        FormField(value: $uname, icon: "person.badge.clock.fill", placeholder: "Age")
+                        
+//                    }
+
+                    Button(action: {}){
+                        HStack{
+                            Image(systemName: "checkmark.circle")
+                            Text("Save").font(.title)
+                        }
+                        .modifier(ButtonModifers())
+                        .padding()
+                    }
+                    
+                }
+//            }
         }
+ 
     }
 }
 
